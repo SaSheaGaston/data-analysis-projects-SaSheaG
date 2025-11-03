a9edf3cf1cca6f8a96d3d17661bc36192625be87
SaShea G <srgme14@gmail.com>
2025-11-02 20:39:18 -0600

    Added answers to each file.

--- loops/exercises/while-loop-exercises.py
+++ loops/exercises/while-loop-exercises.py
@@ -1,4 +1,7 @@
 # Define three variables for the LaunchCode shuttle - one for the starting fuel level, another for the number of astronauts aboard, and the third for the altitude the shuttle reaches.
+fuel_level = 0
+num_astronauts = 0
+shuttle_altitude = 0
 
 
 
@@ -7,19 +10,34 @@
 # Exercise #1: Construct while loops to do the following:
   # a. Query the user for the starting fuel level. Validate that the user enters a positive, integer value greater than 5000 but less than 30000. 
 
-
+while fuel_level  < 5000 or fuel_level >= 30000:
+    fuel_level = int(input("Enter the starting fuel level: "))
 
 
 
 # b. Use a second loop to query the user for the number of astronauts (up to a maximum of 7). Validate the entry.
   
-  
+while num_astronauts <= 0 or num_astronauts > 7:
+    num_astronauts = int(input("Enter number of astronauts: "))
   
   
 # c. Use a final loop to monitor the fuel status and the altitude of the shuttle. Each iteration, decrease the fuel level by 100 units for each astronaut aboard. Also, increase the altitude by 50 kilometers.
 
+while fuel_level - 100 * num_astronauts >= 0:
+    fuel_level -= 100 * num_astronauts
+    shuttle_altitude += 50
 
 
 # Exercise #2: Print the result with the phrase, The shuttle gained an altitude of ___ km and has ___ kg of fuel left. Fill in the blanks with the altitude and fuel level values.
 
 # If the altitude is 2000 km or higher, add “Orbit achieved!” Otherwise add, “Failed to reach orbit.”
+
+
+
+
+if shuttle_altitude >= 2000:
+    final_note = "Orbit achieved!"
+else:
+    final_note = "Failed to reach orbit."
+
+print("The shuttle gained an altitude of", shuttle_altitude, "km and has", fuel_level, "kg of fuel left.", final_note)
