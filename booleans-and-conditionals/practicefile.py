10d04e59bb6411e02bb2891de94c7c43fd03aeb1
SaShea G <srgme14@gmail.com>
2025-11-02 20:47:40 -0600

    Added answers to each file.

---
+++ booleans-and-conditionals/practicefile.py
@@ -0,0 +1,39 @@
+'''
+answer_1 = 'yes'
+answer_2 = 'no'
+
+
+if answer_1 == 'yes':
+   if answer_2 == 'yes':
+      print("Both of you agree!")
+   else:
+      print("You two need to work this out.")
+else:
+   if answer_2 == 'yes':
+      print("Stop arguing and work it out.")
+   else:
+      print("Clean your bathroom anyway!")
+'''
+'''
+num = 8
+
+if num % 2 == 0:
+   print("Launch")
+elif num > 5:
+   print("Code")
+else:
+   print("LaunchCode")
+'''
+
+'''
+num = -10
+if num < 0:
+   print("The negative number", num, "is not valid here.")
+if num > 0:
+   print(num, "is a positive number")
+else:
+   print(num, "is 0")
+
+'''
+for number in range(10):
+   print("I have", 12 - number, "cookies. I'm going to eat one!")
